f6eaad46412d93eb9224a841923fa76a13900455
Denkaan <dennis.petreski@hotmail.com>
2021-09-23 21:30:02 +0200

    navbar fix and other small changes

--- Project/app.js
+++ Project/app.js
@@ -1,7 +1,22 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
-menu.addEventListener('click', function() {
+const mobileMenu = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-});
+};
+
+
+menu.addEventListener('click', mobileMenu);
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if(window.innerWidth <= 800 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);

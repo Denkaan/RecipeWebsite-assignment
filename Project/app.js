87bf8a2ea7d900d0f77d10693ce657b5a8fd50a3
Denkaan <dennis.petreski@hotmail.com>
2021-09-24 09:32:19 +0200

    smooth scroll

--- Project/app.js
+++ Project/app.js
@@ -77,30 +77,89 @@ const hideMobileMenu = () => {
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
 
-document.getElementById("navbar__logo").onclick = function () {
-    location.href = "#home";
+
+var EPPZScrollTo =
+{
+    documentVerticalScrollPosition: function()
+    {
+        if (self.pageYOffset) return self.pageYOffset;
+        if (document.documentElement && document.documentElement.scrollTop) return document.documentElement.scrollTop;
+        if (document.body.scrollTop) return document.body.scrollTop;
+        return 0;
+    },
+
+    viewportHeight: function()
+    { return (document.compatMode === "CSS1Compat") ? document.documentElement.clientHeight : document.body.clientHeight; },
+
+    documentHeight: function()
+    { return (document.height !== undefined) ? document.height : document.body.offsetHeight; },
+
+    documentMaximumScrollPosition: function()
+    { return this.documentHeight() - this.viewportHeight(); },
+
+    elementVerticalClientPositionById: function(id)
+    {
+        var element = document.getElementById(id);
+        var rectangle = element.getBoundingClientRect();
+        return rectangle.top;
+    },
+
+    scrollVerticalTickToPosition: function(currentPosition, targetPosition)
+    {
+        var filter = 0.2;
+        var fps = 60;
+        var difference = parseFloat(targetPosition) - parseFloat(currentPosition);
+
+        var arrived = (Math.abs(difference) <= 0.5);
+        if (arrived)
+        {
+            scrollTo(0.0, targetPosition);
+            return;
+        }
+
+        currentPosition = (parseFloat(currentPosition) * (1.0 - filter)) + (parseFloat(targetPosition) * filter);
+
+        scrollTo(0.0, Math.round(currentPosition));
+
+        setTimeout("EPPZScrollTo.scrollVerticalTickToPosition("+currentPosition+", "+targetPosition+")", (1000 / fps));
+    },
+
+    scrollVerticalToElementById: function(id, padding)
+    {
+        var element = document.getElementById(id);
+        if (element == null)
+        {
+            console.warn('Cannot find element with id \''+id+'\'.');
+            return;
+        }
+
+        var targetPosition = this.documentVerticalScrollPosition() + this.elementVerticalClientPositionById(id) - padding;
+        var currentPosition = this.documentVerticalScrollPosition();
+
+        var maximumScrollPosition = this.documentMaximumScrollPosition();
+        if (targetPosition > maximumScrollPosition) targetPosition = maximumScrollPosition;
+
+        this.scrollVerticalTickToPosition(currentPosition, targetPosition);
+    }
 };
+
+document.getElementById("navbar__logo").onclick = function () {
+  EPPZScrollTo.scrollVerticalToElementById('home', 100)};
 
 document.getElementById("footer__logo").onclick = function () {
-    location.href = "#home";
-};
+  EPPZScrollTo.scrollVerticalToElementById('home', 100)};
 
 document.getElementById("breakfast__navbar").onclick = function () {
-    location.href = "#breakfast";
-};
+  EPPZScrollTo.scrollVerticalToElementById('breakfast', 100)};
 
 document.getElementById("dinner__navbar").onclick = function () {
-    location.href = "#dinner";
-};
+  EPPZScrollTo.scrollVerticalToElementById('dinner', 100)};
 
 document.getElementById("dessert__navbar").onclick = function () {
-    location.href = "#dessert";
-};
+  EPPZScrollTo.scrollVerticalToElementById('dessert', 100)};
 
 document.getElementById("footer__navbar").onclick = function () {
-    location.href = "#footer";
-};
+  EPPZScrollTo.scrollVerticalToElementById('special', 100)};
 
 document.getElementById("special__navbar").onclick = function () {
-    location.href = "#special";
-};
+  EPPZScrollTo.scrollVerticalToElementById('footer', 100)};
